0d2ae1d4f3e1118e9cce3bbaa0e07905c63c95dc
”Volodymyr” <snehvova@gmail.com>
2024-01-17 17:16:36 +0100

    fix some bugs with creating a new file

--- src/SupportClasses/FileCreator.java
+++ src/SupportClasses/FileCreator.java
@@ -5,12 +5,19 @@ import java.io.IOException;
 
 public class FileCreator {
     public File createNewFile(File file, Enum mode){
-        final String currentFileName = file.getName();
-        final String absolutePath = file.getAbsolutePath();
-        final String absolutePathOfNewFile = null;
-        String nameForNewFiles = null;
-
+        String currentFileName = file.getName();
         int dotIndex = currentFileName.lastIndexOf(".");
+        int openParenIndex = currentFileName.lastIndexOf("(");
+
+        if(openParenIndex != -1){
+            currentFileName = currentFileName.substring(0 , openParenIndex) + currentFileName.substring(dotIndex);
+            dotIndex = currentFileName.lastIndexOf(".");
+        }
+
+
+        final String absolutePath = file.getAbsolutePath();
+
+        String nameForNewFiles = null;
 
         if(dotIndex != -1){
             nameForNewFiles = currentFileName.substring(0, dotIndex) + "(" + mode + ")" + currentFileName.substring(dotIndex);
